bf7eb41c8fab6a8a4551fb5204a7626b0f57ccaf
iyadjamal <iyadjamal5@gmail.com>
2017-08-03 04:09:47 +0300

    iyad

--- app/src/main/java/com/example/iyadjamal/iyadsmemo/MainActivity.java
+++ app/src/main/java/com/example/iyadjamal/iyadsmemo/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.iyadjamal.iyadsmemo;
 
 import android.content.pm.ActivityInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        double x;
+
     }
 }
